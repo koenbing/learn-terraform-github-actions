80ba1178ddf49d76157d3b73258de4de77cebacf
Conrad Achidi <koenbing@conrads-mbp.lan>
2022-01-24 12:52:07 -0500

    Point backend to correct TFC and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Koen-demo"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "dev-github-actions"
     }
   }
 }
